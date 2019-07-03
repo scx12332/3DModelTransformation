d1da374b72cf7ab3ff3e60cfdea3ec32deb9fd59
scx <545602511@qq.com>
2019-07-03 15:30:29 +0800

    init repo

---
+++ src/main/java/com/model/modeltransformation/ModelTransformationApplication.java
@@ -0,0 +1,13 @@
+package com.model.modeltransformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelTransformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelTransformationApplication.class, args);
+    }
+
+}
